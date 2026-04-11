d364e9453d0782579c0ee30ba58f59b17179b57c
ahmniab <ahmniab11@gmail.com>
2026-04-12 01:18:36 +0200

    feat: added eShop.WebApp microservice

---
+++ src/microservices/eShop.WebApp/WebApp/GlobalUsings.cs
@@ -0,0 +1,19 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Net.Http;
+global using System.Net.Http.Json;
+global using System.Threading.Tasks;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using eShop.EventBus;
+global using eShop.EventBus.Abstractions;
+global using eShop.EventBusRabbitMQ;
+
+global using eShop.WebApp.Components;
+global using eShop.WebApp.Services;
+global using eShop.ServiceDefaults;
