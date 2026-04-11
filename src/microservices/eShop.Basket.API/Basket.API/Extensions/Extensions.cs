653a00e0a7ad43629bb9d6c9a7c7becf4fed3608
ahmniab <ahmniab11@gmail.com>
2026-04-12 01:05:08 +0200

    feat: eShop.Basket.API

---
+++ src/microservices/eShop.Basket.API/Basket.API/Extensions/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using eShop.Basket.API.Repositories;
+using eShop.Basket.API.IntegrationEvents.EventHandling;
+using eShop.Basket.API.IntegrationEvents.EventHandling.Events;
+
+namespace eShop.Basket.API.Extensions;
+
+public static class Extensions
+{
+    public static void AddApplicationServices(this IHostApplicationBuilder builder)
+    {
+        builder.AddDefaultAuthentication();
+
+        builder.Services.AddSingleton<IConnectionMultiplexer>(_ =>
+            ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("Redis")!));
+
+        builder.Services.AddSingleton<IBasketRepository, RedisBasketRepository>();
+
+        builder.Services.AddRabbitMqEventBus(builder.Configuration)
+                        .AddSubscription<OrderStartedIntegrationEvent, OrderStartedIntegrationEventHandler>();
+    }
+}
---
+++ src/microservices/eShop.Basket.API/Basket.UnitTests/BasketServiceTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Security.Claims;
+using System.Threading;
+using eShop.Basket.API.Repositories;
+using eShop.Basket.API.Grpc;
+using eShop.Basket.API.Model;
+using eShop.Basket.UnitTests.Helpers;
+using Microsoft.Extensions.Logging.Abstractions;
+using BasketItem = eShop.Basket.API.Model.BasketItem;
+
+namespace eShop.Basket.UnitTests;
+
+[TestClass]
+public class BasketServiceTests
+{
+    [TestMethod]
+    public async Task GetBasketReturnsEmptyForNoUser()
+    {
+        var mockRepository = Substitute.For<IBasketRepository>();
+        var service = new BasketService(mockRepository, NullLogger<BasketService>.Instance);
+        var serverCallContext = TestServerCallContext.Create(cancellationToken: CancellationToken.None);
+        serverCallContext.SetUserState("__HttpContext", new DefaultHttpContext());
+
+        var response = await service.GetBasket(new GetBasketRequest(), serverCallContext);
+
+        Assert.IsInstanceOfType<CustomerBasketResponse>(response);
+        Assert.AreEqual(0, response.Items.Count);
+    }
+
+    [TestMethod]
+    public async Task GetBasketReturnsItemsForValidUserId()
+    {
+        var mockRepository = Substitute.For<IBasketRepository>();
+        List<BasketItem> items = [new BasketItem { Id = "some-id" }];
+        mockRepository.GetBasketAsync("1").Returns(Task.FromResult(new CustomerBasket { BuyerId = "1", Items = items }));
+        var service = new BasketService(mockRepository, NullLogger<BasketService>.Instance);
+        var serverCallContext = TestServerCallContext.Create(cancellationToken: CancellationToken.None);
+        var httpContext = new DefaultHttpContext();
+        httpContext.User = new ClaimsPrincipal(new ClaimsIdentity([new Claim("sub", "1")]));
+        serverCallContext.SetUserState("__HttpContext", httpContext);
+
+        var response = await service.GetBasket(new GetBasketRequest(), serverCallContext);
+
+        Assert.IsInstanceOfType<CustomerBasketResponse>(response);
+        Assert.AreEqual(1, response.Items.Count);
+    }
+
+    [TestMethod]
+    public async Task GetBasketReturnsEmptyForInvalidUserId()
+    {
+        var mockRepository = Substitute.For<IBasketRepository>();
+        List<BasketItem> items = [new BasketItem { Id = "some-id" }];
+        mockRepository.GetBasketAsync("1").Returns(Task.FromResult(new CustomerBasket { BuyerId = "1", Items = items }));
+        var service = new BasketService(mockRepository, NullLogger<BasketService>.Instance);
+        var serverCallContext = TestServerCallContext.Create(cancellationToken: CancellationToken.None);
+        var httpContext = new DefaultHttpContext();
+        serverCallContext.SetUserState("__HttpContext", httpContext);
+
+        var response = await service.GetBasket(new GetBasketRequest(), serverCallContext);
+
+        Assert.IsInstanceOfType<CustomerBasketResponse>(response);
+        Assert.AreEqual(0, response.Items.Count);
+    }
+}
